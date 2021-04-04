b5cf4234d7b92053de1112e096d210c2a274c88b
Leandro <lggcontas@gmail.com>
2021-04-04 11:10:34 -0300

    Feat: Configurações iniciais

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'linebreak-style': 0,
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 0,
+    'no-undef': ['off'],
+    camelcase: ['off'],
+    'no-restricted-syntax': ['off'],
+    'no-param-reassign': ['off'],
+    'no-use-before-define': ['off'],
+    'no-plusplus': ['off'],
+    'func-names': ['off'],
+    'max-len': [
+      'warn',
+      {
+        code: 80,
+        tabWidth: 2,
+        comments: 80,
+        ignoreComments: false,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+  },
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,38 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Head from 'next/head';
 
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: 'Quicksand', sans-serif;
   }
-`
+`;
 
 const theme = {
   colors: {
     primary: '#0070f3',
+    rose: '#F6C8CA',
+    lightGreen: '#A6DAD8',
+    darkGreen: '#609694',
+    baseGray: '#AAABB0',
+    red: '#AA4545',
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+      </Head>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
