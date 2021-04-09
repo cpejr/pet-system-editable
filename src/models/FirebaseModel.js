b8ff6354bc055c1f4797c1ec1ae7f40b7c29537c
Matheus <matheusssb@ufmg.br>
2021-04-08 22:24:02 -0300

    Fit:Ajustando firebase config

--- src/models/FirebaseModel.js
+++ src/models/FirebaseModel.js
@@ -3,21 +3,23 @@ const firebase = require('firebase/app');
 
 require('firebase/auth');
 
-// const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_APIKEY,
-//   authDomain: process.env.FIREBASE_AUTHDOMAIN,
-//   databaseURL: process.env.FIREBASE_DATABASEURL,
-//   projectId: process.env.FIREBASE_PROJECTID,
-//   storageBucket: process.env.FIREBASE_STORAGEBUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGINGSEND,
-//   appId: process.env.FIREBASE_APPID,
-// };
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASEURL,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSEND,
+  appId: process.env.FIREBASE_APPID,
+};
 
-// firebase.initializeApp(firebaseConfig);
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   // Inserir o databaseURL
-// });
+if (!firebase.apps.length) {
+  try {
+    firebase.initializeApp(firebaseConfig);
+  } catch (err) {
+    console.error('Firebase initialization error raised', err.stack);
+  }
+}
 
 module.exports = {
   async createNewUser(email, password) {
