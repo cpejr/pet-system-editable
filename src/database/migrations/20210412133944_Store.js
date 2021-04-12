a1f1d743d966133d4aa16ae154b6906ff498add5
Giovanni Martins <martinsgio95@gmail.com>
2021-04-12 11:09:52 -0300

    Feat:  Migration da Loja

---
+++ src/database/migrations/20210412133944_Store.js
@@ -0,0 +1,24 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('Store', (table) => {
+    table.string('store_id').primary().notNullable();
+    table.string('user_id').notNullable();
+    table.foreign('user_id').references('firebase_id').inTable('Users').onDelete('cascade');
+    table.string('company_name').notNullable();
+    table.string('email').notNullable();
+    table.integer('telephone').notNullable();
+    table.integer('cellphone').notNullable();
+    table.integer('cnpj').notNullable();
+    table.integer('cep').notNullable();
+    table.integer('ie').notNullable();
+    table.string('ie_state').notNullable();
+    table.string('cover_img').notNullable();
+    table.string('logo_img').notNullable();
+    table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+    table.float('evaluation').notNullable();
+    table.string('string').notNullable();
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('Store');
+};
