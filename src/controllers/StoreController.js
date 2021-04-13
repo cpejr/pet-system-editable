9b0f330908d37b380c8b47230ab45fc5bce28cde
Giovanni Martins <martinsgio95@gmail.com>
2021-04-13 20:27:52 -0300

    Feat: Implementação inicial da Rota e do Controller

--- src/controllers/StoreController.js
+++ src/controllers/StoreController.js
@@ -1,5 +1,6 @@
 const StoreModel = require('../models/StoreModel');
-// const FireBaseModel = require('../models/FirebaseModel');
+const UserModel = require('../models/UserModel');
+const FireBaseModel = require('../models/FirebaseModel');
 
 module.exports = {
   async getOne(request, response) {
@@ -7,9 +8,41 @@ module.exports = {
     const store = await StoreModel.getStoreById(id);
     return response.json(store);
   },
-  /*
-  async create(request, response) {
 
+  async create(request, response) {
+    const user = request.body;
+    const store = request.body;
+    let firebase_id;
+
+    try {
+      // Criacao de usuario:
+      firebase_id = await FireBaseModel.createNewUser(user.email, user.password);
+      user.firebase_id = firebase_id;
+      delete user.password;
+      await UserModel.createNewUser(user);
+
+      // Checa se o usuario e do tipo vendedor:
+      if (user.type === 'vendedor') {
+        // Usuario habilitado a criar loja
+        try {
+          await StoreModel.createNewStore(store);
+        } catch (err) {
+          console.log(err);
+          throw new Error(err);
+        }
+      } else {
+        // Usuario apagado, nao habilitado a criar uma loja
+        FirebaseModel.deleteUser(firebase_id);
+      }
+    } catch (err) {
+      if (firebase_id) {
+        FirebaseModel.deleteUser(firebase_id);
+      }
+      if (err.message) {
+        return response.status(400).json({ notification: err.message });
+      }
+      return response.status(500).json({ notification: 'Internal server error while trying to register user' });
+    }
+    return response.status(200).json({ notification: 'Usuario criado!' });
   },
-  */
 };
