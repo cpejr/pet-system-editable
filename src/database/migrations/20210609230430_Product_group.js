4438976a093f35567a9df5626e5f2b57683c6616
Giovanni Pantuzzo <giovannipantuzzo@cpejr.com.br>
2021-07-31 15:14:58 -0300

    Modificação na tabela product_group
    Adequando tabela ao diagrama

--- src/database/migrations/20210609230430_Product_group.js
+++ src/database/migrations/20210609230430_Product_group.js
@@ -1,7 +1,10 @@
 exports.up = function (knex) {
   return knex.schema.createTable('product_group', (table) => {
-    table.string('group_id').notNullable().references('group_id').inTable('group');
-    table.string('product_id').notNullable().references('product_id').inTable('product');
+    table.string('group_id').notNullable();
+    table.foreign('group_id').references('group_id').inTable('group');
+    table.string('product_id').notNullable();
+    table.foreign('product_id').references('product_id').inTable('product');
+    table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
   });
 };
 
