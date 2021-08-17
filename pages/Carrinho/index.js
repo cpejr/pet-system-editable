894276ce1cd1f5950732f27ba171aaec7a648df4
Gustavo Santos <gustavosantos@cpejr.com.br>
2021-08-17 20:03:02 -0300

    Criação pagina carrinho

---
+++ pages/Carrinho/index.js
@@ -0,0 +1,11 @@
+import Header from "../../src/components/Header"
+import { CarrinhoText } from "../../src/components/CarrinhoComponents"
+
+export default function Carrinho(){
+    return (
+        <>
+            <Header/>
+            <CarrinhoText>Carrinho de Compras</CarrinhoText>
+        </>
+    )
+}
---
+++ src/components/CarrinhoComponents/index.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const CarrinhoText = styled.p`
+color: #609694;
+font-family: Roboto;
+font-size: 1.3em;
+  @media screen and (max-width: 900px) {
+    font-size: 1em;
+  }
+  @media screen and (max-width: 600px) {
+    font-size: 0.8em;
+  }
+`;
