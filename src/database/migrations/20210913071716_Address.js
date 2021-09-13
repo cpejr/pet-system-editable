549d759dd727d1e07facf0af0be9bb074ac3c20b
Miguel <miguelmenezes@cpejr.com.br>
2021-09-13 15:43:27 -0300

    Ajuste
    Troca das aspas por apóstrofo

--- src/database/migrations/20210913071716_Address.js
+++ src/database/migrations/20210913071716_Address.js
@@ -1,20 +1,20 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("Address", (table) => {
-    table.uuid("address_id").primary().notNullable();
-    table.string("firebase_id").notNullable();
-    table.foreign("firebase_id").references("firebase_id").inTable("User");
-    table.foreign("firebase_id").references("firebase_id_store").inTable("Store");
-    table.string("zipcode").notNullable();
-    table.string("state").notNullable();
-    table.string("city").notNullable();
-    table.string("street").notNullable();
-    table.string("district").notNullable();
-    table.string("address_num").notNullable();
-    table.string("complement").notNullable(); 
-    table.timestamp("created_at").notNullable().defaultTo(knex.fn.now());
+  return knex.schema.createTable('Address', (table) => {
+    table.uuid('address_id').primary().notNullable();
+    table.string('firebase_id').notNullable();
+    table.foreign('firebase_id').references('firebase_id').inTable('User');
+    table.foreign('firebase_id').references('firebase_id_store').inTable('Store');
+    table.string('zipcode').notNullable();
+    table.string('state').notNullable();
+    table.string('city').notNullable();
+    table.string('street').notNullable();
+    table.string('district').notNullable();
+    table.string('address_num').notNullable();
+    table.string('complement').notNullable();
+    table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
   });
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTable("Address");
+  return knex.schema.dropTable('Address');
 };
