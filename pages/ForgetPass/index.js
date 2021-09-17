ad9180d14ba93ad12b45fa7f0d332053eed7a02b
Miguel <miguelmenezes@cpejr.com.br>
2021-09-16 22:06:21 -0300

    ForgetPass
    Envio de email funcionando

--- pages/ForgetPass/index.js
+++ pages/ForgetPass/index.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import Image from 'next/image';
+import { notification } from 'antd';
 import 'date-fns';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'antd/dist/antd.css';
@@ -12,9 +13,47 @@ import {
 import {
   TitleLogin, SubtitleLogin, TextBox, Submit, ForgotPassword, CreateAccount, Divider,
 } from '../../src/components/FormComponents';
+import { useAuth } from '../../src/contexts/AuthContext';
 
-const ForgetPass = () => (
-  <>
+const ForgetPass = () => {
+  const [email, setEmail] = useState('');
+  const { forgottenPassword } = useAuth();
+
+/*eslint-disable*/
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+  }
+  async function sendResetEmail(event) {
+    event.preventDefault();
+    try {
+      await forgottenPassword(email);
+      notification.open({
+        message: 'Sucesso!',
+        description:
+            'Email enviado com sucesso.',
+        className: 'ant-notification',
+        top: '100px',
+        style: {
+          width: 600,
+        },
+      });
+     
+    } catch (error) {
+      console.log(error);
+      notification.open({
+        message: 'Erro!',
+        description:
+            'Erro ao enviar email de redefinição usuário.',
+        className: 'ant-notification',
+        top: '100px',
+        style: {
+          width: 600,
+        },
+      });
+    }
+  }
+  return (
+    <>
     <Header />
 
     <Body>
@@ -25,19 +64,21 @@ const ForgetPass = () => (
       <Divider width="1" display="block" size="300" />
 
       <Body.Right>
+      <Formulary onSubmit={sendResetEmail}>
         <TopFormulary>
           <TitleLogin>Esqueceu sua senha?</TitleLogin>
           <SubtitleLogin>Por favor, entre com seu email para enviarmos um email de redefinição</SubtitleLogin>
           <ItemFormulary>
-            <TextBox type="text" placeholder="Email" />
+            <TextBox type="text" placeholder="Email" onChange={handleEmailChange} value={email}/>
           </ItemFormulary>
           <BottomFormulary>
             <Submit type="submit">Enviar</Submit>
           </BottomFormulary>
         </TopFormulary>
+        </Formulary>
       </Body.Right>
     </Body>
   </>
-);
-
+  );
+};
 export default ForgetPass;
