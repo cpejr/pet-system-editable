ff8e6fb179b67941a38b0a6a6597aa314de51a3c
Miguel <miguelmenezes@cpejr.com.br>
2021-09-22 16:57:56 -0300

    Barra de sublinhado

--- pages/User/Perfil/MyRequests/index.js
+++ pages/User/Perfil/MyRequests/index.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Link from 'next/link';
 import Header from '../../../../src/components/Header';
@@ -11,88 +11,116 @@ import FooterMobile from '../../../../src/components/Mobile/FooterMobile';
 import BlackBars from '../../../../src/components/BlackBars';
 
 const Title = styled.h1`
-display:flex;
-align-items:initial;
-margin-left:5%;
-margin-top:2%;
-margin-bottom:1%;
-font-family: Quicksand;
-@media(max-width:560px){
-    display:flex;
-    align-items:center;
-    justify-content:center;
-    margin-bottom:2%;
-    }
+  display: flex;
+  align-items: initial;
+  margin-left: 5%;
+  margin-top: 2%;
+  margin-bottom: 1%;
+  font-family: Quicksand;
+  @media (max-width: 560px) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 2%;
+  }
 `;
 
 const SubTitle = styled.div`
-display:flex;
-align-items:initial;
-margin-left:8%;
-font-family: Quicksand;
-@media(max-width:560px){
-    display:flex;
-    align-items:center;
-    justify-content:center;
-    margin:0;
-    }
+  display: flex;
+  align-items: initial;
+  margin-left: 8%;
+  font-family: Quicksand;
+  @media (max-width: 560px) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0;
+  }
 `;
 const Section = styled.button`
-display:flex;
-font-size:16px;
-align-items:center;
-justify-content:center;
-margin-right:1%;
-margin-left:1%;
-border:none;
-background-color:${({ theme }) => theme.colors.background};
-cursor: pointer;
-font-family: Quicksand;
-outline:none;
+  display: flex;
+  font-size: 16px;
+  align-items: center;
+  justify-content: center;
+  margin-right: 1%;
+  margin-left: 1%;
+  border: none;
+  background-color: ${({ theme }) => theme.colors.background};
+  cursor: pointer;
+  font-family: Quicksand;
+  outline: none;
+`;
+const Section2 = styled.button`
+  display: flex;
+  font-size: 16px;
+  align-items: center;
+  justify-content: center;
+  margin-right: 1%;
+  margin-left: 1%;
+  border: none;
+  background-color: ${({ theme }) => theme.colors.background};
+  cursor: pointer;
+  font-family: Quicksand;
+  outline: none;
+`;
+const Section3 = styled.button`
+  display: flex;
+  width:max;
+  flex-direction: column;
+  font-size: 16px;
+  align-items: center;
+  justify-content: center;
+  margin-right: 1%;
+  margin-left: 1%;
+  border: none;
+  background-color: ${({ theme }) => theme.colors.background};
+  cursor: pointer;
+  font-family: Quicksand;
+  outline: none;
 `;
 
 Section.Select = styled.button`
-flex-direction:row;
-font-size: 16px;
-align-items:center;
-margin-left:1%;
-margin-right:1%;
-border:none;
-background-color:${({ theme }) => theme.colors.hoverBackground};
-border-radius:5%;
-outline:none;
-font-family:  Quicksand;
+  flex-direction: row;
+  font-size: 16px;
+  align-items: center;
+  margin-left: 1%;
+  margin-right: 1%;
+  border: none;
+  background-color: ${({ theme }) => theme.colors.hoverBackground};
+  border-radius: 5%;
+  outline: none;
+  font-family: Quicksand;
 `;
 const ActualType = styled.h2`
-display:flex;
-align-items:initial;
-margin-left:5%;
-font-family: Quicksand;
-@media(max-width:560px){
-font-size:18px;
-margin-top:10%;
-    }
+  display: flex;
+  align-items: initial;
+  margin-left: 5%;
+  font-family: Quicksand;
+  @media (max-width: 560px) {
+    font-size: 18px;
+    margin-top: 10%;
+  }
 `;
 const OldType = styled.h2`
-display:flex;
-align-items:initial;
-margin-left:5%;
-margin-top:3%;
-font-family: Quicksand;
-@media(max-width:560px){
-font-size:18px;
-margin-top:10%;
-    }
+  display: flex;
+  align-items: initial;
+  margin-left: 5%;
+  margin-top: 3%;
+  font-family: Quicksand;
+  @media (max-width: 560px) {
+    font-size: 18px;
+    margin-top: 10%;
+  }
 `;
-const VerticalBars = styled.h2`
+const VerticalBars = styled.div`
     background-color: black;
     width: 1px;
     height: 40px;
     }
 `;
-const HorizontalBars = styled.h2`
+const HorizontalBars = styled.div`
     background-color: black;
-    width: max;
+    width: 568px;
     height: 1px;
     }
 `;
@@ -101,37 +129,39 @@ export default function Perfil() {
   return (
     <div>
       <Header />
-      <Title>Meu Perfil:</Title>
+      <Title>Meu Perfil</Title>
 
       <SubTitle>
-        <Link href="http://localhost:3000/User/Perfil/MyRequests">
-          <Section.Select>Meus pedidos</Section.Select>
-        </Link>
-        <VerticalBars></VerticalBars>
-        <Link href="http://localhost:3000/User/Perfil/MyAdresses">
-          <Section>Meus endereços</Section>
-        </Link>
-        <VerticalBars></VerticalBars>
-        <Link href="http://localhost:3000/User/Perfil/MyDatas">
-          <Section>Dados pessoais</Section>
-        </Link>
-        <VerticalBars></VerticalBars>
-        <Link href="http://localhost:3000/User/Perfil/ChangePassword">
-          <Section>Alterar Senha</Section>
-        </Link>
-        <VerticalBars></VerticalBars>
-        <Section>
-          <MyLogOut />
-        </Section>
-      <HorizontalBars></HorizontalBars>
+        <Section3>
+          <Section2>
+            <Link href="http://localhost:3000/User/Perfil/MyRequests">
+              <Section.Select>Meus pedidos</Section.Select>
+            </Link>
+            <VerticalBars />
+            <Link href="http://localhost:3000/User/Perfil/MyAdresses">
+              <Section>Meus endereços</Section>
+            </Link>
+            <VerticalBars />
+            <Link href="http://localhost:3000/User/Perfil/MyDatas">
+              <Section>Dados pessoais</Section>
+            </Link>
+            <VerticalBars />
+            <Link href="http://localhost:3000/User/Perfil/ChangePassword">
+              <Section>Alterar Senha</Section>
+            </Link>
+            <VerticalBars />
+            <Section>
+              <MyLogOut />
+            </Section>
+          </Section2>
+        <HorizontalBars></HorizontalBars>
+        </Section3>
       </SubTitle>
-      <HorizontalBars></HorizontalBars>
       <ActualType>Pedido Atual</ActualType>
-      <MyListOrders/>
+      <MyListOrders />
       <OldType>Pedidos Anteriores</OldType>
-      <MyListFinalizedOrders/>
+      <MyListFinalizedOrders />
       <FooterMobile />
-
     </div>
   );
 }
