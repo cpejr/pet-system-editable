384f155e7455739a6f0d1aadcc30e76f1f6a0208
Miguel <miguelmenezes@cpejr.com.br>
2021-09-22 22:12:58 -0300

    Padronização do Menu do Perfil do Usuário

--- pages/User/Perfil/ChangePassword/index.js
+++ pages/User/Perfil/ChangePassword/index.js
@@ -4,37 +4,36 @@ import Link from 'next/link';
 import Header from '../../../../src/components/Header';
 import MyChangePassword from '../../../../src/components/MyChangePassword';
 import MyLogOut from '../../../../src/components/MyLogOut';
-import WordsDivider from '../../../../src/components/WordsDivider';
 import FooterMobile from '../../../../src/components/Mobile/FooterMobile';
 
 const Title = styled.h1`
-align-items:initial;
-display:flex;
-margin-left:5%;
-margin-top:2%;
-margin-bottom:1%;
-font-family: Quicksand;
-@media(max-width:560px){
-    display:flex;
-    align-items:center;
-    justify-content:center;
-    margin-bottom:2%;
-    }
+  display: flex;
+  align-items: initial;
+  margin-left: 5%;
+  margin-top: 2%;
+  margin-bottom: 1%;
+  font-family: Quicksand;
+  @media (max-width: 560px) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 2%;
+  }
 `;
 
 const SubTitle = styled.div`
-align-items:initial;
-display:flex;
-margin-left:8%;
-font-family: Roboto;
-@media(max-width:560px){
-  align-items:center;
-  justify-content:center;
-  margin:0;
-}
+  display: flex;
+  align-items: initial;
+  margin-left: 8%;
+  font-family: Quicksand;
+  @media (max-width: 560px) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0;
+  }
 `;
-
-const ContainerChangePassword= styled.div`
+const ContainerChangePassword = styled.div`
 display:flex;
 flex-direction: column;
 justify-content:space-around;
@@ -47,64 +46,69 @@ justify-content:center;
 align-content:center;
 margin-top:3%;
 `;
-
 const Section = styled.button`
-display:flex;
-font-size:16px;
-align-items:center;
-justify-content:center;
-border:none;
-margin-left:1%;
-margin-right:1%;
-background-color:${({ theme }) => theme.colors.background};
-cursor: pointer;
-font-family: Roboto;
-outline:none;
+  display: flex;
+  font-size: 16px;
+  align-items: center;
+  justify-content: center;
+  margin-right: 1%;
+  margin-left: 1%;
+  border: none;
+  background-color: ${({ theme }) => theme.colors.background};
+  cursor: pointer;
+  font-family: Quicksand;
+  outline: none;
 `;
 
 Section.Select = styled.button`
-flex-direction:row;
-font-size:16px;
-align-items:center;
-margin-left:1%;
-border:none;
-margin-right:1%;
-background-color:${({ theme }) => theme.colors.hoverBackground};
-border-radius:5%;
-outline:none;
-font-family: Roboto;
+  flex-direction: row;
+  font-size: 16px;
+  align-items: center;
+  margin-left: 1%;
+  margin-right: 1%;
+  border: none;
+  background-color: ${({ theme }) => theme.colors.hoverBackground};
+  border-radius: 5%;
+  outline: none;
+  font-family: Quicksand;
+`;
+const VerticalBars = styled.div`
+    background-color: black;
+    width: 1px;
+    height: 40px;
+    }
 `;
 
 export default function Perfil() {
   return (
     <ContainerChangePassword>
       <Header />
-      <Title>Meu Perfil:</Title>
+      <Title>Meu Perfil</Title>
 
       <SubTitle>
         <Link href="http://localhost:3000/User/Perfil/MyRequests">
           <Section>Meus pedidos</Section>
         </Link>
-        <WordsDivider />
+        <VerticalBars />
         <Link href="http://localhost:3000/User/Perfil/MyAdresses">
           <Section>Meus endereços</Section>
         </Link>
-        <WordsDivider />
+        <VerticalBars />
         <Link href="http://localhost:3000/User/Perfil/MyDatas">
           <Section>Dados pessoais</Section>
         </Link>
-        <WordsDivider />
+        <VerticalBars />
         <Link href="http://localhost:3000/User/Perfil/ChangePassword">
           <Section.Select>Alterar Senha</Section.Select>
         </Link>
-        <WordsDivider />
+        <VerticalBars />
         <Section>
           <MyLogOut />
         </Section>
 
       </SubTitle>
       <MyQuack>
-         <MyChangePassword />
+        <MyChangePassword />
       </MyQuack>
       <FooterMobile />
 
--- pages/User/Perfil/MyRequests/index.js
+++ pages/User/Perfil/MyRequests/index.js
@@ -1,14 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import Link from 'next/link';
 import Header from '../../../../src/components/Header';
-import MyRequest from '../../../../src/components/MyRequest';
 import MyListOrders from '../../../../src/components/MyListOrders';
 import MyListFinalizedOrders from '../../../../src/components/MyListFinalizedOrders';
 import MyLogOut from '../../../../src/components/MyLogOut';
-import WordsDivider from '../../../../src/components/WordsDivider';
 import FooterMobile from '../../../../src/components/Mobile/FooterMobile';
-import BlackBars from '../../../../src/components/BlackBars';
 
 const Title = styled.h1`
   display: flex;
@@ -50,34 +47,6 @@ const Section = styled.button`
   font-family: Quicksand;
   outline: none;
 `;
-const Section2 = styled.button`
-  display: flex;
-  font-size: 16px;
-  align-items: center;
-  justify-content: center;
-  margin-right: 1%;
-  margin-left: 1%;
-  border: none;
-  background-color: ${({ theme }) => theme.colors.background};
-  cursor: pointer;
-  font-family: Quicksand;
-  outline: none;
-`;
-const Section3 = styled.button`
-  display: flex;
-  width:max;
-  flex-direction: column;
-  font-size: 16px;
-  align-items: center;
-  justify-content: center;
-  margin-right: 1%;
-  margin-left: 1%;
-  border: none;
-  background-color: ${({ theme }) => theme.colors.background};
-  cursor: pointer;
-  font-family: Quicksand;
-  outline: none;
-`;
 
 Section.Select = styled.button`
   flex-direction: row;
@@ -91,40 +60,12 @@ Section.Select = styled.button`
   outline: none;
   font-family: Quicksand;
 `;
-const ActualType = styled.h2`
-  display: flex;
-  align-items: initial;
-  margin-left: 5%;
-  font-family: Quicksand;
-  @media (max-width: 560px) {
-    font-size: 18px;
-    margin-top: 10%;
-  }
-`;
-const OldType = styled.h2`
-  display: flex;
-  align-items: initial;
-  margin-left: 5%;
-  margin-top: 3%;
-  font-family: Quicksand;
-  @media (max-width: 560px) {
-    font-size: 18px;
-    margin-top: 10%;
-  }
-`;
 const VerticalBars = styled.div`
     background-color: black;
     width: 1px;
     height: 40px;
     }
 `;
-const HorizontalBars = styled.div`
-    background-color: black;
-    width: 568px;
-    height: 1px;
-    }
-`;
-
 export default function Perfil() {
   return (
     <div>
@@ -132,34 +73,29 @@ export default function Perfil() {
       <Title>Meu Perfil</Title>
 
       <SubTitle>
-        <Section3>
-          <Section2>
-            <Link href="http://localhost:3000/User/Perfil/MyRequests">
-              <Section.Select>Meus pedidos</Section.Select>
-            </Link>
-            <VerticalBars />
-            <Link href="http://localhost:3000/User/Perfil/MyAdresses">
-              <Section>Meus endereços</Section>
-            </Link>
-            <VerticalBars />
-            <Link href="http://localhost:3000/User/Perfil/MyDatas">
-              <Section>Dados pessoais</Section>
-            </Link>
-            <VerticalBars />
-            <Link href="http://localhost:3000/User/Perfil/ChangePassword">
-              <Section>Alterar Senha</Section>
-            </Link>
-            <VerticalBars />
-            <Section>
-              <MyLogOut />
-            </Section>
-          </Section2>
-        <HorizontalBars></HorizontalBars>
-        </Section3>
+        <Link href="http://localhost:3000/User/Perfil/MyRequests">
+          <Section.Select>Meus pedidos</Section.Select>
+        </Link>
+        <VerticalBars />
+        <Link href="http://localhost:3000/User/Perfil/MyAdresses">
+          <Section>Meus endereços</Section>
+        </Link>
+        <VerticalBars />
+        <Link href="http://localhost:3000/User/Perfil/MyDatas">
+          <Section>Dados pessoais</Section>
+        </Link>
+        <VerticalBars />
+        <Link href="http://localhost:3000/User/Perfil/ChangePassword">
+          <Section>Alterar Senha</Section>
+        </Link>
+        <VerticalBars />
+        <Section>
+          <MyLogOut />
+        </Section>
       </SubTitle>
-      <ActualType>Pedido Atual</ActualType>
+      <Title>Pedido Atual</Title>
       <MyListOrders />
-      <OldType>Pedidos Anteriores</OldType>
+      <Title>Pedidos Anteriores</Title>
       <MyListFinalizedOrders />
       <FooterMobile />
     </div>
