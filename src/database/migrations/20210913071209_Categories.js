762bfc515849e9f9bd41c1e0e178f9422caf60e4
Matheus <matheusmiranda98@live.com>
2021-09-23 12:56:41 -0300

    Imagem na tabela categories e identação do card carrinho

--- src/database/migrations/20210913071209_Categories.js
+++ src/database/migrations/20210913071209_Categories.js
@@ -2,6 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('Categories', (table) => {
     table.uuid('category_id').primary().notNullable();
     table.string('name').notNullable();
+    table.string('img').notNullable();
   });
 };
 
