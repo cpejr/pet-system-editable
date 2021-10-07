557d2e1adb2773e4562db14edde7712d09a1e50e
Miguel <miguelmenezes@cpejr.com.br>
2021-10-07 13:01:39 -0300

    Mudança na ID visual do Card do produto

--- src/components/SearchCards/index.js
+++ src/components/SearchCards/index.js
@@ -1,30 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { AiFillHeart, AiFillStar } from 'react-icons/ai';
+import api from '../../utils/api';
 import {
   CardWrapper, CardInfo, CardDescription, CardDescriptionTitle,
-  CardDescriptionValues, CardDescriptionDeliveryPrice, CardButtons,
-  FavButton, Button, Star,
+  CardDescriptionValues, CardDescriptionDeliveryPrice,
 } from './styles';
 
 export default function SearchCards(props) {
   const { product } = props;
-  const [checkedFav1, setCheckedFav1] = useState('#C4C4C4');
-  const handleClickFav1 = () => {
-    if (checkedFav1 === '#C4C4C4') {
-      setCheckedFav1('#F6C8CA');
-    } else {
-      setCheckedFav1('#C4C4C4');
-    }
-  };
+  const [store, setStore] = useState([]);
+  useEffect(() => {
+    api.get(`store/${product.firebase_id_store}`).then((res) => {
+      setStore(res.data);
+    });
+  }, []);
 
   const myLoader = ({ src }) => `https://s3-sa-east-1.amazonaws.com/petsystembucket/${src}`;
 
   return (
     <Link href={`/Product/${product.product_id}`}>
       <CardWrapper>
-        <Image loader={myLoader} src={product.img} alt="" width="100%" height="100%" />
+        <Image loader={myLoader} src={product.img} alt="" width="200" height="200" />
         <CardInfo>
           <CardDescription>
             <CardDescriptionTitle>
@@ -32,25 +29,14 @@ export default function SearchCards(props) {
             </CardDescriptionTitle>
             <CardDescriptionValues>
               <CardDescriptionDeliveryPrice>
-                •Valor:
+                R$
+                {' '}
                 {product.price}
+                {' '}
+                {store.shipping_tax}
               </CardDescriptionDeliveryPrice>
-              <Star>
-                <AiFillStar />
-                5.0
-              </Star>
             </CardDescriptionValues>
           </CardDescription>
-          <CardButtons>
-            <Button>5.0</Button>
-            <FavButton>
-              <AiFillHeart
-                size={24}
-                onClick={handleClickFav1}
-                style={{ color: checkedFav1 }}
-              />
-            </FavButton>
-          </CardButtons>
         </CardInfo>
       </CardWrapper>
     </Link>
--- src/components/SearchCards/styles.js
+++ src/components/SearchCards/styles.js
@@ -1,16 +1,18 @@
 import styled from 'styled-components';
 
 export const CardWrapper = styled.button`
-display: flex;
-flex-direction: column;
-align-items: center;
-width: 100%;
-padding: 1vw;
-height: auto;
-font-family:Roboto;
-background-color: transparent;
-border: none;
-cursor: pointer;
+  display: flex;
+  align-items: center;
+  width: auto;
+  padding: 1vw;
+  height: auto;
+  font-family: Roboto;
+  background-color: transparent;
+  border: 1px solid #f2f2f2;
+  border-radius: 4px;
+  cursor: pointer;
+  text-align: justify;
+  text-justify: inter-word;
   &:hover {
     box-shadow: 0 16px 40px 0px rgba(112, 144, 176, 0.2);
     transform: scale(1.02);
@@ -18,97 +20,54 @@ cursor: pointer;
 `;
 
 export const CardInfo = styled.div`
-display:flex;
-align-items:center;
-justify-content:space-around;
-padding: 3%;
-width:100%;
-font-size:16px;
-flex-direction:row;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  padding: 3%;
+  width: 100%;
+  font-size: 16px;
+  flex-direction: row;
+  border-left: 1px solid #f2f2f2;
+  margin-left: 5%;
 `;
 
 export const CardDescription = styled.div`
-display:flex;
-align-items:center;
-justify-content:center;
-width: 50%;
-flex-direction:column;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: auto;
+  flex-direction: column;
 `;
 
 export const CardDescriptionTitle = styled.h3`
-display:flex;
-align-items:center;
-justify-content:flex-start;
-width:100%;
-margin-top:1%;
-margin-bottom:1%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+  margin-top: 1%;
+  margin-bottom: 1%;
 `;
 
 export const CardDescriptionValues = styled.p`
-display:flex;
-align-items:center;
-justify-content:flex-start;
-width:100%;
-margin:0;
-flex-direction:row;
-@media(max-width:880px){
-    flex-direction:column;
-    align-items:initial;
-}
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+  margin: 0;
+  flex-direction: row;
+  @media (max-width: 880px) {
+    flex-direction: column;
+    align-items: initial;
+  }
 `;
 export const CardDescriptionDeliveryPrice = styled.div`
-display:flex;
-align-items:center;
-justify-content:flex-start;
-width:60%;
-font-size:14px;
-@media(max-width:560px){
-    width:100%;
-    color:${({ theme }) => theme.colors.baseGray};
-}
-`;
-
-export const CardButtons = styled.div`
-display:flex;
-align-items:center;
-justify-content:center;
-width:15%;
-@media(max-width:560px){
-width:30%;
-}
-`;
-
-export const FavButton = styled.button`
-display:none;
-@media(max-width:560px){
-display:flex;
-align-items:center;
-justify-content:center;
-border:none;
-background-color:${({ theme }) => theme.colors.background};
-}
-`;
-
-export const Button = styled.button`
-display:flex;
-align-items:center;
-justify-content:center;
-border-radius:50%;
-width:35px;
-height:35px;
-background-color:${({ theme }) => theme.colors.mediumGray};
-border:none;
-@media(max-width:560px){
-display:none;
-}
-
-`;
-
-export const Star = styled.div`
-display:none;
-@media(max-width:560px){
-    display:flex;
-    color:${({ theme }) => theme.colors.starYellow};
-    margin-left:5%;
-}
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  width: 60%;
+  font-size: 24px;
+  @media (max-width: 560px) {
+    width: 100%;
+    color: ${({ theme }) => theme.colors.baseGray};
+  }
 `;
