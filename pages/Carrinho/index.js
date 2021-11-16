cb07f252c392fb62dd9b250045ba72283b203193
Miguel <miguelmenezes@cpejr.com.br>
2021-11-16 14:30:19 -0300

    Ajuste na exibição dos produtos no carrinho

--- pages/Carrinho/index.js
+++ pages/Carrinho/index.js
@@ -1,67 +1,70 @@
 import React, { useEffect, useState } from 'react';
 import { MdShoppingCart } from 'react-icons/md';
-import { useAuth } from '../../src/contexts/AuthContext';
 import {
-  CarrinhoBody,
-  CarrinhoFinalButton,
-  CarrinhoIcon,
-  CarrinhoText,
-  CarrinhoTitle,
-  CarrinhoTotal,
-  CarrinhoValor,
-  CarrinhoValorText,
+  CarrinhoBody, CarrinhoFinalButton, CarrinhoIcon, CarrinhoText,
+  CarrinhoTitle, CarrinhoTotal, CarrinhoValor, CarrinhoValorText,
+  CarrinhoValorTitle,
 } from '../../src/components/CarrinhoComponents';
+import CarrinhoFrete from '../../src/components/CarrinhoComponents/CarrinhoFrete';
 import CarrinhoCard from '../../src/components/CarrinhoComponents/CarrinhoCard';
-
+import { useAuth } from '../../src/contexts/AuthContext';
+import { ContainerDatas, BoxDatas } from '../../src/components/MyAdresses/styles';
 import api from '../../src/utils/api';
 
 export default function Carrinho() {
   const [products, setProducts] = useState([]);
   const [subTotal, setSubTotal] = useState(0);
   const { user } = useAuth();
+
   useEffect(() => {
     if (user) {
-      api.get(`/cart/firebase/${user.firebase_id}`).then((response) => {
-        console.log(response);
+      console.log(user);
+      api.get('/cart/firebase').then((res) => {
+        console.log(res.data);
+        setProducts(res.data);
       });
-      // api.get(`/CartProducts/${cart.cart_id}`).then((res) => {
-      //   setProducts(res.data);
-      // });
     }
   }, [user]);
-
+  if (products.length > 0) {
+    return (
+      <>
+        <CarrinhoTitle>
+          <CarrinhoText>Carrinho de Compras</CarrinhoText>
+          <CarrinhoIcon>
+            <MdShoppingCart size="100%" color="#609694" />
+          </CarrinhoIcon>
+        </CarrinhoTitle>
+        <CarrinhoBody>
+          <div>
+            {products.map((p) => (
+              <CarrinhoCard product={p} key={p.product_id} setSubTotal={setSubTotal} subTotal={subTotal} />
+            ))}
+          </div>
+          <CarrinhoValor>
+            <CarrinhoValorTitle>Digite o seu CEP</CarrinhoValorTitle>
+            <CarrinhoFrete />
+            <CarrinhoTotal>
+              <CarrinhoValorText>SubTotal</CarrinhoValorText>
+              <CarrinhoValorText>
+                R$
+                {subTotal}
+              </CarrinhoValorText>
+              <CarrinhoValorText>Frete</CarrinhoValorText>
+              <CarrinhoValorText>R$Frete</CarrinhoValorText>
+              <CarrinhoValorText>Total</CarrinhoValorText>
+              <CarrinhoValorText>R$Total</CarrinhoValorText>
+            </CarrinhoTotal>
+            <CarrinhoFinalButton>Continuar</CarrinhoFinalButton>
+          </CarrinhoValor>
+        </CarrinhoBody>
+      </>
+    );
+  }
   return (
-    <>
-      <CarrinhoTitle>
-        <CarrinhoText>Carrinho de Compras</CarrinhoText>
-        <CarrinhoIcon>
-          <MdShoppingCart size="100%" color="#609694" />
-        </CarrinhoIcon>
-      </CarrinhoTitle>
-      <CarrinhoBody>
-        <div>
-          {products.map((p) => (
-            <CarrinhoCard
-              product={p}
-              key={p.product_id}
-              setSubTotal={setSubTotal}
-              subTotal={subTotal}
-            />
-          ))}
-        </div>
-        <CarrinhoValor>
-          <CarrinhoTotal>
-            <CarrinhoValorText>SubTotal</CarrinhoValorText>
-            <CarrinhoValorText>
-              R$
-              {subTotal}
-            </CarrinhoValorText>
-            <CarrinhoValorText>Total</CarrinhoValorText>
-            <CarrinhoValorText>R$Total</CarrinhoValorText>
-          </CarrinhoTotal>
-          <CarrinhoFinalButton>Continuar</CarrinhoFinalButton>
-        </CarrinhoValor>
-      </CarrinhoBody>
-    </>
+    <ContainerDatas>
+      <BoxDatas>
+        <p>Nenhum produto em seu carrinho</p>
+      </BoxDatas>
+    </ContainerDatas>
   );
 }
