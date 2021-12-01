0832143d3393297c684d50c7045bd09cf2d0c4fc
Giovanni Pantuzzo <giovannipantuzzo@cpejr.com.br>
2021-12-01 18:07:14 -0300

    Push to take the repair of many connections

--- src/components/PerfilMenu/index.js
+++ src/components/PerfilMenu/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Link from 'next/link';
 import MyLogOut from '../MyLogOut';
 import { SubTitle, Section, StyledSpan } from './styles';
+import api from '../../utils/api';
+import JsonToCSV from '../JsonToCSV';
 
 const menuItens = [
   {
@@ -22,7 +24,22 @@ const menuItens = [
   },
 ];
 
+// const testando = [
+//   {
+//     company_name: 'DESGRAÇA', cnpj: '112155445', email: 'é isso porra', cellphone: '112155445', phone: '112155445', shipping_tax: 'R$5,00',
+//   },
+//   {
+//     email: 'é isso porra', cnpj: '112155445', cellphone: '112155445', phone: '112155445', shipping_tax: 'R$5,00', company_name: 'CARALHO',
+//   },
+// ];
+
 export default function PerfilMenu({ selectedItem }) {
+  const getStoreData = async () => {
+    const { data } = await api.get('store');
+    console.log('🚀 ~ file: index.js ~ line 39 ~ getStoreData ~ teste', data);
+    return data;
+  };
+
   return (
     <div>
       <SubTitle>
@@ -39,6 +56,9 @@ export default function PerfilMenu({ selectedItem }) {
             </Link>
           </>
         ))}
+        <Section style={{ border: 'none' }}>
+          <JsonToCSV data={getStoreData()} />
+        </Section>
         <Section style={{ border: 'none' }}>
           <MyLogOut />
         </Section>
