e839e199763fe0441c34f902a7fbba53176a1d44
Matheus <matheusmiranda98@live.com>
2021-12-08 13:06:33 -0300

    Implementando Checkout PagSeguro

---
+++ pages/api/payCheckout/index.js
@@ -0,0 +1,15 @@
+import { createCheckoutPagSeguro } from '../../../src/controllers/OrderController';
+import { withAuthValidation } from '../../../src/utils/Auth';
+
+export default function handler(req, res) {
+  try {
+    const { method } = req;
+    console.log('paycreateCheckoutPagSeguro', method);
+    if (method === 'POST') {
+      return withAuthValidation(createCheckoutPagSeguro)(req, res);
+    }
+    return res.status(500).json({ message: 'Internal Server Error' });
+  } catch (err) {
+    return res.status(500).json({ statusCode: 500, message: err.message });
+  }
+}
