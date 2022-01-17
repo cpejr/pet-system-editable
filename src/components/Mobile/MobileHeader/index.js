1eadb98e7ca84c9c7fc6d7e3977503f430da8932
Julia Tarchi Maia de Souza <jutarchi00@gmail.com>
2022-01-17 13:38:59 -0300

    Started making header's buttons functional

--- src/components/Mobile/MobileHeader/index.js
+++ src/components/Mobile/MobileHeader/index.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 import { MdHome, MdReceipt } from 'react-icons/md';
 import { BsFillPersonFill } from 'react-icons/bs';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { MobileHeaderContainer, MobileHeaderSpace } from './styles';
+import Link from 'next/link';
+import { useAuth } from '../../../contexts/AuthContext';
 
 export default function MobileHeader() {
+  const {user, store} = useAuth();
+  const router = useRouter();
+  const [searchText, setSearchText] = useState('');
+
+  const ProfileButton = () => {
+    if (!user && !store) router.push('/login');
+    if (store) router.push('/Seller/Perfil/Products');
+    if (user && user.type != 'Admin') router.push('/User/Perfil/MyRequests');
+    if (user.type === 'Admin') router.push('/Admin');
+  };
+
+  const HomeButton = () => router.push('/Home');
+
+  const handleSubmit = () => router.push({ pathname: '/Search', query: { keyword: searchText } });
+
+  const handleFilterSearchText = (e) => setSearchText(e.target.value);
+
+  const handleKeypress = (e) => {
+    // it triggers by pressing the enter key
+    if (e.key === 'Enter') {
+      handleSubmit();
+    }
+  };
+
   return (
     <div>
       <MobileHeaderSpace />
       <MobileHeaderContainer>
-        <MobileHeaderContainer.Col1>
+        <MobileHeaderContainer.Col1 onClick={HomeButton}>
           <MdHome size="40" />
           Início
         </MobileHeaderContainer.Col1>
@@ -21,7 +48,7 @@ export default function MobileHeader() {
           <MdReceipt size="40" />
           Pedidos
         </MobileHeaderContainer.Col3>
-        <MobileHeaderContainer.Col4>
+        <MobileHeaderContainer.Col4 onClick={ProfileButton}>
           <BsFillPersonFill size="40" />
           Perfil
         </MobileHeaderContainer.Col4>
