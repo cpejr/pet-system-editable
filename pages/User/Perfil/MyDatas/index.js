e79e1f0882b602c58712d27e76625793b6946a4e
Giovanni Pantuzzo <giovannipantuzzo@cpejr.com.br>
2022-01-29 17:54:33 -0300

    Implementing verify functions of buyer page permission

--- pages/User/Perfil/MyDatas/index.js
+++ pages/User/Perfil/MyDatas/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import {
   Title, PerfilMenu, MyDatas,
 } from '../../../../src/components';
+import withAuthUser from '../../../../src/components/WithAuth/WithAuthUser';
 
-export default function Perfil() {
-  return (
-    <div>
-      <Title>Meu Perfil</Title>
-      <PerfilMenu selectedItem="Meus Dados" />
-      <Title>Informações Pessoais</Title>
-      <MyDatas />
-    </div>
-  );
-}
+const Perfil = () => (
+  <div>
+    <Title>Meu Perfil</Title>
+    <PerfilMenu selectedItem="Meus Dados" />
+    <Title>Informações Pessoais</Title>
+    <MyDatas />
+  </div>
+);
+
+export default withAuthUser(Perfil);
