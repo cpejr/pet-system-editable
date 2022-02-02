a2bfdbbb849afbe1ff9164e3fcf8db6e01149118
Julia Tarchi Maia de Souza <jutarchi00@gmail.com>
2022-02-02 09:49:03 -0300

    Updating profit

--- src/models/OrderModel.js
+++ src/models/OrderModel.js
@@ -137,7 +137,20 @@ module.exports = {
     try {
       const response = await connection('Admin_share')
         .select('*').first();
-      const profit = (response.share * order.total_price/100);
+      let somaPrecos = 0;
+      if (user) {
+        api.get('/cart/firebase').then((res) => {
+          if (res.data.length > 0) {
+            res.data.forEach((product) => {
+              somaPrecos += product.price * product.amount;
+            });
+          }
+        })
+      }
+      console.log(somaPrecos);
+      const profit = (response.share * somaPrecos/100);
+      console.log(profit);
+      order.admin_profit = profit;
       const order_aux = await connection('Order')
         .insert(order);
       return order_aux;
