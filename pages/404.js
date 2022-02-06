f1a712d158a842eae52dd96279d6bf70a39e057d
JuliaTMSouza <jutarchi00@gmail.com>
2022-02-06 17:31:32 -0300

    Fixed responsivity

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,3 @@
-/* eslint-disable */ 
 import React from 'react';
 import styled from 'styled-components';
 
@@ -17,11 +16,20 @@ justify-content: center;
   }
 `;
 
+const Animation = styled.div`
+@media (max-width: 600px) {
+  width: 60%;
+  }
+`;
+
 const Title = styled.div`
 color: black;
 font-weight: 600;
 font-size: 3em;
 margin-bottom: 5%;
+@media (max-width: 800px) {
+    font-size: 2em;
+  }
 @media (max-width: 600px) {
     font-size: 2em;
   }
@@ -31,6 +39,7 @@ const Text = styled.div`
 flex-direction: column;
 justify-content: center;
 margin-right:3%;
+width:75%;
 @media (max-width: 600px) {
     margin-left: 3%;
   }
@@ -41,8 +50,11 @@ margin-bottom:2%;
 color: black;
 font-weight: 300;
 font-size: 1.5em;
+@media(max-width: 800px) {
+    font-soze:1.2em;
+  }
 @media (max-width: 600px) {
-    font-size: 1.5em;
+    font-size: 1.2em;
   }
 
 `;
@@ -50,14 +62,14 @@ font-size: 1.5em;
 export default function NonExistingPage() {
   return (
     <IsntFound>
-      <div>
+      <Animation>
         <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js" />
         <lottie-player src="https://assets6.lottiefiles.com/private_files/lf30_uqcbmc4h.json" speed="1" autoplay background="transparent" />
-      </div>
+      </Animation>
       <Text>
         <Title>Pagina não encontrada</Title>
-        <Content>Não foi possível encontrar a página que você está procurando, isso ocorre porque:</Content> 
-        <Content>- Há um erro no URL em seu navegador da wev. Verifique o URL e tente novamente.</Content>
+        <Content>Não foi possível encontrar a página que você está procurando, isso ocorre porque:</Content>
+        <Content>- Há um erro no URL em seu navegador da web. Verifique o URL e tente novamente.</Content>
         <Content>- A página que você está procurando foi movida ou excluída.</Content>
       </Text>
     </IsntFound>
