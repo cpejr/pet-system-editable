11d93925e32fb0a609f47709d50bec8f2fff8b24
Nikole <kolenick7n@hotmail.com>
2022-02-10 03:01:48 -0300

    Time changes on back

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -12,59 +12,75 @@ export async function signIn(req, res) {
   try {
     const { email, password } = req.body;
     let firebase_id;
-
-    try {
-      firebase_id = await FirebaseModel.login(email, password);
-      const user = await UserModel.getUserById(firebase_id);
-      const storeStatus = await StoreModel.getStatusByEmail(email);
-      const attempt = await AttemptsModel.getAttemptByEmail(email);
-
-      if (attempt.attempts === 0) {
-        const body = {
-          lock_time: moment().add(5, 'minutes'),
-          attempts: attempt.attempts + 1,
-        };
-        await AttemptsModel.updateAttempt(body, email);
-      } else {
-        switch (attempt.attempts) {
-          case 1: {
-            const body = {
-              attempts: attempt.attempts + 1,
-            };
-            await AttemptsModel.updateAttempt(body, email);
-          }
-          // eslint-disable-next-line no-fallthrough
-          case 2: {
-            const body = {
-              attempts: attempt.attempts + 1,
-            };
-            await AttemptsModel.updateAttempt(body, email);
-            break;
-          }
-          default: {
-            // eslint-disable-next-line no-unused-vars
-            const body = {
-              attempts: attempt.attempts + 1,
-            };
-          }
+    const attempt = await AttemptsModel.getAttemptByEmail(email);
+    if (attempt.attempts === 0) {
+      const body = {
+        lock_time: moment().add(5, 'minutes'),
+        attempts: attempt.attempts + 1,
+      };
+      await AttemptsModel.updateAttempt(body, email);
+    } else {
+      switch (attempt.attempts) {
+        case 1: {
+          const body = {
+            attempts: attempt.attempts + 1,
+          };
+          await AttemptsModel.updateAttempt(body, email);
+        }
+        // eslint-disable-next-line no-fallthrough
+        case 2: {
+          const body = {
+            attempts: attempt.attempts + 1,
+          };
+          await AttemptsModel.updateAttempt(body, email);
+          break;
+        }
+        case 3: {
+          const body = {
+            attempts: attempt.attempts + 1,
+          };
+          await AttemptsModel.updateAttempt(body, email);
+          break;
+        }
+        case 4: {
+          const body = {
+            attempts: attempt.attempts + 1,
+          };
+          await AttemptsModel.updateAttempt(body, email);
+          break;
+        }
+        default: {
+          // eslint-disable-next-line no-unused-vars
+          const body = {
+            attempts: attempt.attempts + 1,
+          };
         }
       }
-      if (attempt.attempts === 3 && moment() <= moment(attempt.lock_time)) {
-        const body = {
-          lock_time: moment().add(5, 'minutes'),
-        };
-        await AttemptsModel.updateAttempt(body, attempt.email);
+      if (attempt.attempts >= 3 && attempt.attempts !== 5
+        && moment() <= moment(attempt.lock_time)) {
         return res.status(200).json('Bloqueado');
       }
-      if (attempt.attempts === 3 && moment() > moment(attempt.lock_time)) {
+      if (attempt.attempts >= 3 && moment() > moment(attempt.lock_time)) {
         const body = {
           lock_time: moment().add(5, 'minutes'),
           attempts: 0,
         };
         await AttemptsModel.updateAttempt(body, email);
       }
-      if (attempt.attempts !== 3) {
-        if (user) {
+      if (attempt.attempts === 5 && moment() <= moment(attempt.lock_time)) {
+        const body = {
+          lock_time: moment(attempt.lock_time).add(5, 'minutes'),
+        };
+        await AttemptsModel.updateAttempt(body, email);
+        return res.status(200).json('Bloqueado');
+      }
+    }
+    try {
+      firebase_id = await FirebaseModel.login(email, password);
+      const user = await UserModel.getUserById(firebase_id);
+      const storeStatus = await StoreModel.getStatusByEmail(email);
+      if (user) {
+        if (attempt.attempts <= 3) {
           const accessToken = jwt.sign(
             { user },
             process.env.NEXT_PUBLIC_JWT_SECRET,
@@ -74,7 +90,10 @@ export async function signIn(req, res) {
             user,
             accessToken,
           });
-
+          const body = {
+            attempts: 0,
+          };
+          await AttemptsModel.updateAttempt(body, email);
           await req.session.save();
 
           return res.status(200).json({ accessToken, user });
