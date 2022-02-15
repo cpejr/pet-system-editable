dc1d5cd997aaab1267bcfbb32c133653d0624038
Matheus <matheusmiranda98@live.com>
2022-02-15 12:33:38 -0300

    change foreign

--- src/database/migrations/20220215144952_Login_attempts.js
+++ src/database/migrations/20220215144952_Login_attempts.js
@@ -1,8 +1,6 @@
 exports.up = function (knex) {
   return knex.schema.createTable('Login_attempts', (table) => {
-    table.string('email').primary().notNullable();
-    table.foreign('email', 'email_Store').references('email').inTable('Store').onDelete('cascade');
-    table.foreign('email', 'email_User').references('email').inTable('User').onDelete('cascade');
+    table.string('email').primary().unique().notNullable();
     table.integer('attempts').notNullable().defaultTo(0);
     table.timestamp('lock_time').notNullable().defaultTo(knex.fn.now());
   });
