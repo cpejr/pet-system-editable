9aaa3ccfc73bfd1772c9dab46d842c44a18f333c
Matheus <matheusmiranda98@live.com>
2022-02-24 11:35:06 -0300

    _error and api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'https://petsystem.vercelapp.com/api' : 'http://localhost:3000/api',
+  baseURL: process.env.NODE_ENV === 'production' ? 'https://petmkt.vercel.app/api' : 'http://localhost:3000/api',
 });
 
 export default api;
